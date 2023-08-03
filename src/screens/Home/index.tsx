fb1752e9b490bda3e92ea4cd9c609955f3dccea7
Pedro-Emanuel <pedroems.147@gmail.com>
2023-08-02 21:23:30 -0300

    Esculhambei o q Manel fez

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,15 @@
-import { Container } from "./styles";
+import { Container, TopBar, Section, Image, Text } from "./styles"; 
 
 export function Home() {
     return(
         <Container>
-            
+            {/* Barra do topo */}
+            <TopBar>
+                <Section>
+                    <Image source={require("../../../assets/icon.png")}/>
+                    <Text>Caio Rian</Text>
+                </Section>
+            </TopBar>
         </Container>
     );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,39 @@
 import styled from 'styled-components/native';
+import theme from '../../theme/index';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: #171A21;
+    background-color: ${theme.COLORS.GRAY_700};
     align-items: center;
     justify-content: 'center';
+`;
+
+export const TopBar = styled.View`
+    background-color: ${theme.COLORS.GRAY_500};
+    width: 100%;
+    height: 25%;
+    border-bottom-left-radius: 30px;
+    border-bottom-right-radius: 30px;
+    shadow-color: #000;
+    elevation: 8;
+`;
+
+export const Section = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-top:52px;
+    margin-left: 32px;
+`;
+
+export const Image = styled.Image`
+    width: 42px;
+    height: 42px;
+    border-radius: 50px;
+`;
+
+export const Text = styled.Text`
+    color: ${theme.COLORS.WHITE};
+    font-size: ${theme.FONT_SIZE.LG};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    margin-left: 16px;
 `;
